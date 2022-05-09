e2669fb48dbc098325aecf08a301baaf80db0991
mbpozzi <mpozzi@usc.edu>
2022-05-09 15:28:45 -0700

    Update server

--- stats/test_server.js
+++ stats/test_server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const { MlbApi } = require('/home/user/ee547/project/mlb_api.js')
+const { MlbApi } = require('/home/user/ee547/project/stats/mlb_api.js')
 const mlb = new MlbApi();
 
 const bodyparser = require('body-parser');
@@ -27,6 +27,20 @@ app.get('/test', (req,res) => {
 
 });
 
+app.get('/team', (req, res,next) => {
+    try{ 
+        mlb.getTeamsStats('2022', (err,data) =>{
+            //console.log(data);
+            res.status(200).json(data);
+            res.end();
+            
+        })
+    } catch(err){
+        res.status(400);
+        res.end();
+    }
+    next();
+})
 app.listen(PORT)
 
 console.log("Server started")
